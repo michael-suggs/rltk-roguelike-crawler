eeb49798e821c43a3992eed1c3122e8fc98866c8
Michael Suggs <suggs.michael.jeffrey@gmail.com>
2021-04-01 05:28:38 -0400

    Control structure for drunkard's walk.

--- src/map_builder/drunkard.rs
+++ src/map_builder/drunkard.rs
@@ -10,12 +10,18 @@ use super::common::{
 };
 use super::{MapBuilder, Map, TileType, Position};
 
+#[derive(PartialEq, Copy, Clone)]
+pub enum DrunkSpawnMode { StartingPoint, Random }
+
+pub struct DrunkardSettings { pub spawn_mode: DrunkSpawnMode }
+
 pub struct DrunkardsWalkBuilder {
     map: Map,
     starting_position: Position,
     depth: i32,
     history: Vec<Map>,
     noise_areas: HashMap<i32, Vec<usize>>,
+    settings: DrunkardSettings,
 }
 
 impl MapBuilder for DrunkardsWalkBuilder {
@@ -54,13 +60,14 @@ impl MapBuilder for DrunkardsWalkBuilder {
 }
 
 impl DrunkardsWalkBuilder {
-    pub fn new(new_depth: i32) -> DrunkardsWalkBuilder {
+    pub fn new(new_depth: i32, settings: DrunkardSettings) -> DrunkardsWalkBuilder {
         DrunkardsWalkBuilder {
             map: Map::new(new_depth),
             starting_position: Position { x: 0, y: 0 },
             depth: new_depth,
             history: Vec::new(),
             noise_areas: HashMap::new(),
+            settings,
         }
     }
 
--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -3,8 +3,9 @@ use specs::prelude::World;
 use bsp_dungeon::BspDungeonBuilder;
 use bsp_interior::BspInteriorBuilder;
 use cellular_automata::CellularAutomataBuilder;
-use drunkard::DrunkardsWalkBuilder;
+use drunkard::{DrunkardsWalkBuilder, DrunkardSettings, DrunkSpawnMode};
 use simple_map::SimpleMapBuilder;
+
 use super::{
     components::Position,
     map::*
@@ -34,6 +35,9 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
         // 2 => Box::new(BspDungeonBuilder::new(new_depth)),
         // 3 => Box::new(BspInteriorBuilder::new(new_depth)),
         // 4 => Box::new(CellularAutomataBuilder::new(new_depth)),
-        _ => Box::new(DrunkardsWalkBuilder::new(new_depth)),
+        _ => Box::new(DrunkardsWalkBuilder::new(
+            new_depth,
+            DrunkardSettings { spawn_mode: DrunkSpawnMode::StartingPoint },
+        )),
     }
 }
